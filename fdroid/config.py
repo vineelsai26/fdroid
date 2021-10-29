bd00af593507dfc1aa64a5ffd82dd254f945374f
Vineel Sai <mail@vineelsai.com>
2021-10-29 22:32:31 +0530

    changes

---
+++ fdroid/config.py
@@ -0,0 +1,4 @@
+repo_url = "https://fdroid.vineelsai.com/fdroid/repo"
+repo_name = "VsTech F-Droid Repo"
+repo_icon = "repo/icons/logo.png"
+repo_description = "This is a repository of apps to be used with F-Droid. By VsTech https://vineelsai.com"
